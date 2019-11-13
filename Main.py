516b3bc5b2f140e04fcd1cd5b44757a36fbe2aea
Vitor Henrique <vhsouzaeng@gmail.com>
2019-11-13 16:07:53 -0300

    admin page added

--- Main.py
+++ Main.py
@@ -9,25 +9,38 @@ upload_folder = 'static/upload'
 jogos = []
 val = {'access':False}
 
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('Home.html',lista=jogos)
+
 @app.route('/admin/cadastro', methods=['POST','GET'])
 def cadastro():
     if val['access'] == True:
         return render_template('Cadastro.html')
     else:
-        return redirect('/home')
+        return redirect('/')
 
-@app.route('/admin')
+@app.route('/login')
 def login():
     return render_template('Login.html')
 
-@app.route('/admin/validation',methods=['POST'])
+@app.route('/logout')
+def logout():
+    val['access'] = False
+    return redirect('/')
+
+@app.route('/admin',methods=['GET'])
+def admin():
+    return render_template('Admin.html',lista=jogos)
+
+@app.route('/login/validation',methods=['POST'])
 def validation():
     userLogin = request.form['login']
     password = str(request.form['password'])
-    print(userLogin, password)
+    
     if userLogin == 'admin' and password == '12345678':
         val['access'] = True
-        return redirect('/admin/cadastro')
+        return redirect('/admin')
     else:
         return redirect('/home')
 
@@ -39,17 +52,36 @@ def cadastrar():
     nota = request.form['nota']
     src = os.path.join(upload_folder,img.filename)
     infoText = request.form['infoText']
-    index = datetime.now()
+    now = datetime.now()
+    index = now.second
 
     img.save(os.path.join(upload_folder,img.filename))
 
 
     jogo = Jogo(index,name,int(nota),src,infoText)
     jogos.append(jogo)
-    return redirect('/home')
+    return redirect('/admin')
 
-@app.route('/home',methods=['GET'])
-def index():
-    return render_template('Home.html',lista=jogos)
+
+@app.route('/admin/delete',methods=['POST'])
+def delete():
+    index = request.form['id']
+    for i in range(0,len(jogos)):
+        if int(index) == int(jogos[i].index):
+            jogos.pop(i)
+    
+    return redirect('/admin')
+
+@app.route('/admin/update',methods=['POST'])
+def update():
+    index = request.form['id']
+    for i in range(0,len(jogos)):
+        if int(index) == int(jogos[i].index):
+            jogos[i].name = request.form['name']
+            jogos[i].nota = int(request.form['nota'])
+            jogos[i].infoText = request.form['infoText']
+            print(jogos[i].infoText)
+    
+    return redirect('/admin')
 
 app.run(port=8080,debug=True)
