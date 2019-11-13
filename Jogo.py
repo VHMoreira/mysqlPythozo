6d7d7437f7302a203d5e6d7d026908ed7b4b8b7f
aluno <aluno@cesupa.br>
2019-11-13 09:22:49 -0300

    add new routes

--- Jogo.py
+++ Jogo.py
@@ -1,5 +1,6 @@
 class Jogo:
-    def __init__(self,name,nota,src, infoText):
+    def __init__(self,index ,name,nota,src, infoText):
+        self.index = index
         self.name = name
         self.nota = nota
         self.src = src
--- Main.py
+++ Main.py
@@ -1,28 +1,50 @@
 from flask import render_template, request, Flask, redirect
 from Jogo import Jogo
-import Database
+from datetime import datetime
+#import Database
 import os
 
 app = Flask(__name__)
 upload_folder = 'static/upload'
 jogos = []
+val = {'access':False}
 
-@app.route('/cadastro', methods=['POST','GET'])
+@app.route('/admin/cadastro', methods=['POST','GET'])
 def cadastro():
-    return render_template('Cadastro.html')
+    if val['access'] == True:
+        return render_template('Cadastro.html')
+    else:
+        return redirect('/home')
 
-@app.route('/cadastrar',methods=['POST'])
+@app.route('/admin')
+def login():
+    return render_template('Login.html')
+
+@app.route('/admin/validation',methods=['POST'])
+def validation():
+    userLogin = request.form['login']
+    password = str(request.form['password'])
+    print(userLogin, password)
+    if userLogin == 'admin' and password == '12345678':
+        val['access'] = True
+        return redirect('/admin/cadastro')
+    else:
+        return redirect('/home')
+
+
+@app.route('/admin/cadastrar',methods=['POST'])
 def cadastrar():
     img = request.files['img']
     name = request.form['name']
     nota = request.form['nota']
     src = os.path.join(upload_folder,img.filename)
     infoText = request.form['infoText']
+    index = datetime.now()
 
     img.save(os.path.join(upload_folder,img.filename))
 
 
-    jogo = Jogo(name,int(nota),src,infoText)
+    jogo = Jogo(index,name,int(nota),src,infoText)
     jogos.append(jogo)
     return redirect('/home')
 
